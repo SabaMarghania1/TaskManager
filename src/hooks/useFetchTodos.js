260134903da03b9d1952bdbb8f2c9ac80b09702e
saba <marghanias@gmail.com>
2024-09-05 17:04:04 +0400

    test

--- src/hooks/useFetchTodos.js
+++ src/hooks/useFetchTodos.js
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../config/supabaseClient";
+import { useAuth } from "@clerk/clerk-react";
 
 const fetchTodos = async (userId) => {
+  if (!userId) {
+    throw new Error("User ID is required");
+  }
+
   const { data, error } = await supabase
     .from("todos")
     .select("*")
@@ -13,10 +18,14 @@ const fetchTodos = async (userId) => {
 
   return data;
 };
-export const useFetchTodos = (userId) => {
+
+export const useFetchTodos = () => {
+  const { userId, getToken } = useAuth();
+
   return useQuery({
     queryKey: ["todos", userId],
     queryFn: () => fetchTodos(userId),
+    enabled: !!userId,
     staleTime: 1000 * 60 * 5,
     refetchOnWindowFocus: false,
   });
